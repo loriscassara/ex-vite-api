1acbe8c24a7651f6a7242e1feb981e9c6ddf3199
MrElle <cassaralavoro@gmail.com>
2023-11-30 15:52:17 +0100

    add axios in App.vue

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,27 @@
 -->
 
 <script>
-import AppHeader from './components/AppHeader.vue'
+import axios from 'axios' //axios import
+import AppHeader from './components/AppHeader.vue' //AppHeader import
 
 export default {
 	components: {
 		AppHeader
+	},
+	data() {
+		return {
+
+		}
+	},
+	mounted() {
+		this.connectBeer();
+	},
+	methods: {
+		connectBeer() {
+			axios.get(`https://api.openbrewerydb.org/v1/breweries?by_country=scotland&per_page=10`).then(result => {
+				//console.log(result);
+			});
+		}
 	}
 }
 </script>
@@ -17,7 +33,7 @@ export default {
 -->
 
 <template>
-
+	contenuto
 </template>
 
 <!-- 
